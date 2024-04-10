1871a21b83c7f0ef301e40570017895c0e6c2b6b
Eugene 'marengohue' Rebedailo <marengohue@protonmail.com>
2024-04-10 20:54:28 +0200

    Add GH link to essentia

--- src/pages/Essentia.tsx
+++ src/pages/Essentia.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Essentia: React.FC = () => {
   return (
     <div className="container">
-      <p className="essentia-tbd">There will be stuff here eventually, I promise.</p>
+      <p className="essentia-tbd">
+        Soon, there will be a glorious medieval alchemy game here.
+        But for now, visit the <a href="https://github.com/irrelephant-io/essentia-rs">GitHub repo.</a>
+      </p>
     </div>
   );
 };
